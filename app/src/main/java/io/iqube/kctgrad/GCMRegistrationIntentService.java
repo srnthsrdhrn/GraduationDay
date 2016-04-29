52c6d030c30963d042bd526720e4943db0513797
sekar navin <sekar.navin79@gmail.com>
2016-04-29 17:29:07 +0530

    Bro done with the gcm !!

--- app/src/main/java/io/iqube/kctgrad/GCMRegistrationIntentService.java
+++ app/src/main/java/io/iqube/kctgrad/GCMRegistrationIntentService.java
@@ -1,7 +1,9 @@
 package io.iqube.kctgrad;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
